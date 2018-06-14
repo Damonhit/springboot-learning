b7e849267cec779d2468ba9f02c05d62622d9d8a
zhanyunpeng <812354236@qq.com>
2018-06-14 23:46:52 +0800

    test

--- src/main/java/com/zyp/springbootlearning/SpringbootLearningApplication.java
+++ src/main/java/com/zyp/springbootlearning/SpringbootLearningApplication.java
@@ -8,6 +8,5 @@ public class SpringbootLearningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootLearningApplication.class, args);
-        
     }
 }
